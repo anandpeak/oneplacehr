1c264130bd63cbff6f3485d6026cac1013ce2aa4
panda-desu <zr.freeze05@gmail.com>
2024-01-19 15:23:29 +0800

    fixing img doesnt displaying 2

--- src/components/Mainpage/RecProcess.jsx
+++ src/components/Mainpage/RecProcess.jsx
@@ -33,7 +33,10 @@ const RecProcess = () => {
 
           <div className="absolute z-4 right-28 top-[20rem]">
             <div className="relative">
-              <img src={"/vectors/imgBorder.png"} alt="lie" />
+              <img
+                src={`${process.env.PUBLIC_URL}/vectors/imgBorder.png`}
+                alt="lie"
+              />
               <div>
                 <img
                   className="absolute lg:top-10 lg:right-10 lg:p-0 md:top-0 md:right-0 md:p-5 flex justify-center items-center "
